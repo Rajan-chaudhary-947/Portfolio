54ca28b42fffd54e1e6722720c817d41894fadf8
Rajan chaudhary <chaudharyrajan947@gmail.com>
2025-09-18 01:50:01 +0530

    Update About.jsx
    Resume update

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1uHh16HUA_2raS25vpSH0bZXvJ6raSaYy/view?usp=sharing"
+            href="https://drive.google.com/file/d/1HgRFP4EdkaIBrxXOsXPs8X5LK5r0oDS-/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
